69c4a9105da328ff52f4493f7517fb1b1bb09209
Robert Finkley <rfinkley82@gmail.com>
2021-11-02 20:21:03 -0400

    updated left attribute of h3

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const panels = document.querySelectorAll(".panel");
 
+const headers = document.querySelectorAll("h3");
+console.log(headers);
+
+headers.forEach((h3) => {
+  let len = h3.innerText.length;
+  let leftPos = `calc(50% + ${len})`;
+  h3.style.left = leftPos;
+});
+
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
